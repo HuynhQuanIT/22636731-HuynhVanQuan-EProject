eda0ca6fc10886bffbb06c363f0c84193121921d
HuynhQuanIT <22636731.quan@student.iuh.edu.vn>
2025-10-26 00:42:29 +0700

    Update5

--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -9,5 +9,9 @@ router.post("/", isAuthenticated, productController.createProduct);
 router.post("/buy", isAuthenticated, productController.createOrder);
 router.get("/", isAuthenticated, productController.getProducts);
 
+router.get("/:id", isAuthenticated, productController.getId);
+
+
+
 
 module.exports = router;
